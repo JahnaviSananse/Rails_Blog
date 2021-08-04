5e687a57a05cd070fd72bac0a4024786461b54dd
JahnaviSananse <jahnavisananse@gmail.com>
2021-08-04 22:03:12 +0530

    Secure passoword with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_061510) do
+ActiveRecord::Schema.define(version: 2021_08_04_162406) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 2021_08_04_061510) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
